167c83239a4b6dde8b6833c09bc2869538ff5a11
sc16276 <sc16276@student.samk.fi>
2025-10-19 16:03:33 +0300

    Lisätty get_numer() for safe input

--- calculator.py
+++ calculator.py
@@ -2,3 +2,10 @@ def add (x, y): return x + y
 def subtraxt(x, y): return x - y
 def multiply(x, y): return x * y
 def divide(x, y): return x / y if y != 0 else 'Error: Division by zero' 
+
+def get_numer(prompt):
+    while True:
+      try:
+         return float(input(prompt))
+       execpt ValueError:
+          print('Please enter a valid number.')
